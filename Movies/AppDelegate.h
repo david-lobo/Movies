f216084f7752571f160d4066aae29e6c617a135f
David Lobo <david@davidlobo.co.uk>
2016-04-01 13:33:25 +0100

    Initial Commit

---
+++ Movies/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Movies
+//
+//  Created by david lobo on 01/04/2016.
+//  Copyright © 2016 David Lobo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Movies/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Movies
+//
+//  Created by david lobo on 01/04/2016.
+//  Copyright © 2016 David Lobo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
